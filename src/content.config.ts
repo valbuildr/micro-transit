fcda393e640aaf0865d035a71196db1770cd9562
valbuildr <150450259+valbuildr@users.noreply.github.com>
2026-02-23 16:26:11 -0600

    Initial commit

---
+++ src/content.config.ts
@@ -0,0 +1,39 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from 'astro/loaders';
+
+const stores = defineCollection({
+    loader: glob({ pattern: '**/*.json', base: "./src/content/stores" }),
+    schema: z.object({
+        name: z.string(),
+        url: z.string(),
+        latLon: z.tuple([z.number(), z.number()]),
+        address: z.string(),
+        stops: z.object({
+            rapid: z.array(z.object({
+                name: z.string(),
+                latLon: z.tuple([z.number(), z.number()]),
+                routes: z.array(z.object({
+                    label: z.string(),
+                    bg: z.string(),
+                    text: z.string().optional(), // default white
+                    url: z.string(),
+                    notes: z.string().optional(),
+                }))
+            })).optional(),
+            local: z.array(z.object({
+                name: z.string(),
+                latLon: z.tuple([z.number(), z.number()]),
+                routes: z.array(z.object({
+                    label: z.string(),
+                    bg: z.string(),
+                    text: z.string().optional(), // default white
+                    url: z.string(),
+                    notes: z.string().optional(),
+                }))
+            })).optional(),
+        }),
+        debug: z.boolean().optional(),
+    }),
+});
+
+export const collections = { stores };
